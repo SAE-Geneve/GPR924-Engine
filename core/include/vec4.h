ab16120470cbeabeab2c9357922115c28639680a
DESKTOP-CFOLVV6\antho <anthonybarman2003@gmail.com>
2025-10-02 15:31:53 +0200

    add maths in incule in core

--- core/include/vec4.h
+++ core/include/vec4.h
@@ -1,12 +1,16 @@
 #ifndef VEC4_H
 #define VEC4_H
 
+#include <cmath>
+#include <concepts>
+
 namespace core::maths
 {
   template<typename Vector4T, typename T>
   concept IsVector4 = requires(Vector4T v)
   {
-
+    { v.x } -> std::same_as<T&>;
+    { v.y } -> std::same_as<T&>;
   };
 }
 
