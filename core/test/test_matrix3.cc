64b201baccdac6abf3cc2b2537051b18537a5925
Jemo <103411555+Jemow@users.noreply.github.com>
2025-10-06 22:29:47 +0200

    Added Identity static method to matrix3.h

--- core/test/test_matrix3.cc
+++ core/test/test_matrix3.cc
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "maths/matrix3.h"
 
-TEST(Matrix3Determinant, Matrix3) {
+TEST(Matrix3, Determinant) {
   constexpr  std::array<std::array<float, 3>, 3> matrix = {{
     {1, 2, 3},
     {4, 5, 6},
@@ -12,7 +12,7 @@ TEST(Matrix3Determinant, Matrix3) {
   EXPECT_EQ(m.Det(), 0);
 }
 
-TEST(Matrix3Identity, Matrix3) {
+TEST(Matrix3, Identity) {
   auto m = core::maths::Matrix3<int>::Identity();
 
   for (int i = 0; i < 3; ++i)
