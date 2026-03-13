d4717e592eaa21a369176d1b25ca21440b2524a1
EliasFarhan <elias.farhan@gmail.com>
2026-03-13 21:03:11 +0100

    Making Photon compile on Linux

--- common/include/third_party/photon_include.h
+++ common/include/third_party/photon_include.h
@@ -3,7 +3,14 @@
 
 #ifdef ENABLE_NETWORK
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+#pragma clang diagnostic ignored "-Wold-style-cast"
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wdtor-name"
+#pragma clang diagnostic ignored "-Wignored-qualifiers"
 #include "LoadBalancing-cpp/inc/Client.h"
+#pragma clang diagnostic pop
 
 #endif
 
